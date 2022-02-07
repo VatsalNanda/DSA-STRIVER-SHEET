77e753896d9c855b3c309b457c62ea83cc437761
Vatsal Nanda <54484624+VatsalNanda@users.noreply.github.com>
2022-02-07 13:38:50 +0530

    Add files via upload

---
+++ DAY-1-ARRAYS/q5-sort 0's,1's, and 2's/q5-sort 0's,1's, and 2's/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  q5-sort 0's,1's, and 2's
+//
+//  Created by vatsal nanda on 07/02/22.
+//
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+    }
+};
