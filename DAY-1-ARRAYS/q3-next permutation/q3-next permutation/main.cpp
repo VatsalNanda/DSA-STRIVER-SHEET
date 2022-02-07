2726510bb11f28775156a92b4170db3d7a047d81
Vatsal Nanda <54484624+VatsalNanda@users.noreply.github.com>
2022-02-07 13:17:55 +0530

    Add files via upload

---
+++ DAY-1-ARRAYS/q3-next permutation/q3-next permutation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  q3-next permutation
+//
+//  Created by vatsal nanda on 07/02/22.
+//
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        (next_permutation(nums.begin(), nums.end()));
+        
+    }
+};
