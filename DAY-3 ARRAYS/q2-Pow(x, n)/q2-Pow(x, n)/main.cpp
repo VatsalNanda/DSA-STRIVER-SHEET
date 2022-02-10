1d5fdc56da7e7e49d6bb46f87966a5c8057a716f
Vatsal Nanda <54484624+VatsalNanda@users.noreply.github.com>
2022-02-10 18:00:36 +0530

    Add files via upload

---
+++ DAY-3 ARRAYS/q2-Pow(x, n)/q2-Pow(x, n)/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  q2-Pow(x, n)
+//
+//  Created by vatsal nanda on 10/02/22.
+//
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double ans=pow(x,n);
+        return ans;
+        
+    }
+};
