dddd06184268ae04961bcc40a3c3be32181f4b30
Vatsal Nanda <54484624+VatsalNanda@users.noreply.github.com>
2022-02-10 19:13:41 +0530

    Add files via upload

---
+++ DAY-3 ARRAYS/q5-Unique Paths/q5-Unique Paths/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  q5-Unique Paths
+//
+//  Created by vatsal nanda on 10/02/22.
+//
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+int uniquePathsUtil(int m, int n) {
+    if(m == 1 || n == 1) return 1;
+    if(dp[m][n] != -1)
+        return dp[m][n];
+    return dp[m][n] = uniquePathsUtil(m-1, n) + uniquePathsUtil(m, n-1);
+}
+int uniquePaths(int m, int n) {
+    dp.clear();
+    dp.assign(m+1, vector<int>(n+1, -1));
+    return uniquePathsUtil(m, n);
+}
+};
