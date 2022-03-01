c05ffb4c44346208542921708c8d4861b92728e7
Vatsal Nanda <54484624+VatsalNanda@users.noreply.github.com>
2022-03-01 12:02:08 +0530

    Add files via upload

---
+++ DAY-10- RECURSION AND BACKTRACKING/q1-Permutations/q1-Permutations/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  q1-Permutations
+//
+//  Created by vatsal nanda on 01/03/22.
+//
+
+class Solution {
+public:
+    void helper(int index,vector<int>& nums, vector<vector<int>>& ans )
+    {
+        if(index==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=index;i<nums.size();i++)
+        {
+            swap(nums[index],nums[i]);
+            helper(index+1,nums,ans);
+            swap(nums[index],nums[i]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        
+        vector<vector<int>> ans;
+        helper(0,nums,ans);
+        return ans;
+        
+    }
+};
